4627230706216d89d134ae7545fa52644109ea9e
unknown <mkap25@yahoo.com>
2018-01-31 21:42:04 -0500

    made some changes, added heroku

--- app/logic/myjs.js
+++ app/logic/myjs.js
@@ -1,18 +1,14 @@
 $(document).ready(function() {
 
-	// Code that makes modals trigger on click
-	$('.modal-trigger').leanModal();
-
-	// When we click the submit button...
 	$("#submit").click(function(){
 
-		// Checking to see if user input a name & image.
+		
 		if ( $('#nameInput').val() == null || $('#imageInput').val() == null ) {
 			alert("Please fill out all questions!");
 			return false;
 		};
 
-		// Checking to see if user answered all of the survey questions.
+		
 		for (var i = 1; i <= 10; i++) {
 			if ( $("input:radio[name=question"+i+"]:checked").val() == null ) {
 				alert("Please fill out all questions!");
@@ -20,25 +16,24 @@ $(document).ready(function() {
 			};
 		};
 
-		// Creating an array to hold the answers.
+	
 		var scoreArray = [];
 
-		// Looping through the answers and pushing them to the array. 
+		
 		for (var i = 1; i <= 10; i++) {
 			scoreArray.push( $("input:radio[name=question"+i+"]:checked").val() );
 		}
 
-		// Creating an object that stores all of the input given by the user.
+		
 		var person = {
 			name: $('#nameInput').val(),
 			image: $('#imageInput').val(),
 			scores: scoreArray
 		};
 
-		// Storing our current URL in a variable.
 		var currentURL = window.location.origin;
 
-		// Making an AJAX post request. The server calculates the match and sends back the info in the data object.
+		
 		$.post(currentURL + '/api/survey', person).done(function(data){
 
 			// Opens our modal.
